c5bfd8f32108ba1874dba920ab6d20f48f9ee068
khoffan <6210110458@psu.ac.th>
2022-08-10 17:23:01 +0700

    FizzBuzz and Test pass

---
+++ coe_number/FizzBuzz.py
@@ -0,0 +1,8 @@
+#FizzBuzz program
+def get_fizzbizz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return 'FizzBuzz'
+    elif num % 3 == 0:
+        return 'Fizz'
+    elif num % 5 == 0:
+        return 'Buzz'
