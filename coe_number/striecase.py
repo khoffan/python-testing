de53c2984defb49f4eb8b87f548e80391fafaf66
khoffan <6210110458@psu.ac.th>
2022-08-15 16:27:26 +0700

    update star

--- coe_number/striecase.py
+++ coe_number/striecase.py
@@ -1,12 +1,12 @@
 # star pattren
 def stiarcase(number, ch):
-    k = 2 * number - 2
+    k = number 
     for i in range(0, number):
         # process each column
         for j in range(0, k):
             # print space in pyramid
             print(end=" ")
-        k = k - 2
+        k = k-1
         for j in range(0, i + 1):
             # display star
             print(ch, end="")
