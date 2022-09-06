4b42ac4b82253b358981695ce822c59978384750
khoffan <6210110458@psu.ac.th>
2022-09-06 21:45:34 +0700

    reload

--- superFizzbuzz/src/Sufizzbuzz.py
+++ superFizzbuzz/src/Sufizzbuzz.py
@@ -1,30 +1,15 @@
-class FizzBuzz:
-    def __init__(self,name: str):
-        self.name = name
-    
-    def get_name(self)-> str:
-        pass
-
-fizzbuzzs = [
-    FizzBuzz('Fizz'),
-    FizzBuzz('Buzz'),
-    FizzBuzz('FizzBuzz')
-]
-
-def get_fizzbuzz(fizzbuzz: list,n:int):
-    for i in fizzbuzz:
-        if n %3 == 0 and i.name == 'Fizz':
-            return True
-        elif n%5 ==0 and i.name == 'Buzz':
-            return True
-        elif n%5 ==0 and n%3 == 0 and i.name == 'FizzBuzz':
-            return True
-        else:
-            return False
-
-def prints():
-    for i in range(1,20):
-        print("{0} {1}".format(i,get_fizzbuzz(fizzbuzzs,i)))
-        
-prints()          
-    
+def superFizzBuzz(n):
+    if n % 3 == 0:
+        return 'Fizz'
+    elif n % 5 == 0:
+        return 'Fizz'
+    elif n % 3 == 0 and n % 5 == 0:
+        return 'FizzBuzz'
+    if n % 9 == 0:
+        return 'FizzFizz'
+    elif n % 25 == 0:
+        return 'BuzzBuzz'
+    elif n % 9 == 0 and n % 25 == 0:
+        return 'FizzFizzBuzzBuzz'
+    elif n % 3 and n % 5 and n % 9 and n % 25:
+        return 'NoFizzBuzz'
