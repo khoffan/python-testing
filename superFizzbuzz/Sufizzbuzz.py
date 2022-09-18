565a2d4828c0c4c1ef342a0241f1aea3d725f9ae
khoffan <khoffan222@gmail.com>
2022-09-18 14:31:30 +0700

    fizzbuzz

--- superFizzbuzz/Sufizzbuzz.py
+++ superFizzbuzz/Sufizzbuzz.py
@@ -1,24 +1,45 @@
 class FizzBuzz:
-    def __init__(self):
-        self.num = 0
-    def add_number(self,num):
-        self.num += num 
+    def __init__(self,num):
+        self.num = num
+    
+    # def add_number(self,num):
+    #     self.num += num
+
+    def get_Buzz(self):
+        pass
+    def get_fizz(self):
+        pass
     def get_fizzBuzz(self):
         pass
-    
+    def get_allfizzbuzz(self):
+        pass
         
-class Get_fizzbuzz(FizzBuzz):
+class Get_fizz():
+    def get_fizz(self):
+        return 'Fizz'
+
+class Get_Buzz():
+    def get_Buzz(self):
+        return 'Buzz'
+        
+class Get_FizzBuzz():
     def get_fizzBuzz(self):
+        return 'FizzBuzz'
+
+
+class Get_allFizzbuzz(FizzBuzz):
+    def get_allfizzbuzz(self):
         if self.num % 3 == 0 and self.num % 5 == 0:
-            return 'FizzBuzz'
+            fizzbuzz = Get_FizzBuzz()
+            return fizzbuzz.get_fizzBuzz()
         elif self.num % 3 == 0:
-            return 'Fizz'
+            fizz = Get_fizz()
+            return fizz.get_fizz()
         elif self.num % 5 == 0:
-            return 'Buzz'
+            buzz = Get_Buzz()
+            return buzz.get_Buzz()
         else:
-            return 'NoFizzBuzz'
-
-
+            return 'nofizzbuzz'
 # class Get_fizzbuzzsuper(FizzBuzz):
 #     def get_fizzBuzzsuper(self):
 #         if self.num % 9 == 0 and self.num % 25 == 0:
@@ -31,12 +52,12 @@ class Get_fizzbuzz(FizzBuzz):
 #             return 'NoFizzBuzz'
 
 def show_fizzbuzz(n: int):
-    show = FizzBuzz()
-    show.add_number(n)
-    return show.get_fizzBuzz()
+    show = Get_allFizzbuzz(n)
+    # show.add_number(n)
+    return show.get_allfizzbuzz()
 
 
-print("show_fizzBuzz {0}".format(show_fizzbuzz(25)))
+print("show_fizzBuzz {0}".format(show_fizzbuzz(15)))
 # def prints():
 #     for i in range(1,100):
 #         if show_fizzbuzz(i) == 'FizzBuzz':
