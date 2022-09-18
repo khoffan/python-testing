9d85dc85109ed33000a2ef815468759391756c9f
khoffan <khoffan222@gmail.com>
2022-09-18 19:17:46 +0700

    add_superfizzbuzz

--- superFizzbuzz/Sufizzbuzz.py
+++ superFizzbuzz/Sufizzbuzz.py
@@ -1,83 +1,68 @@
 class FizzBuzz:
-    def __init__(self,num):
-        self.num = num
-    
-    # def add_number(self,num):
-    #     self.num += num
+    def __init__(self , *divid):
+        self.divid = divid
 
-    def get_Buzz(self):
-        pass
-    def get_fizz(self):
-        pass
-    def get_fizzBuzz(self):
-        pass
-    def get_allfizzbuzz(self):
-        pass
+    def call_divition(self , num: int):
+        for division in self.divid:
+            if num % division != 0:
+                return None
+        return self.get_fizzbuzz()
+
+    def get_fizzbuzz(self):
+        return 'NoFizzBuzz'
         
-class Get_fizz():
-    def get_fizz(self):
+class Get_fizz(FizzBuzz):
+    def __init__(self):
+        super().__init__(3)
+    def get_fizzbuzz(self):
         return 'Fizz'
 
-class Get_Buzz():
-    def get_Buzz(self):
+class Get_Buzz(FizzBuzz):
+    def __init__(self):
+        super().__init__(5)
+    def get_fizzbuzz(self):
         return 'Buzz'
         
-class Get_FizzBuzz():
-    def get_fizzBuzz(self):
-        return 'FizzBuzz'
+class Get_FizzBuzz(FizzBuzz):
+    def __init__(self):
+        super().__init__(3,5)
+
+    def get_fizzbuzz(self):
+        return 'FizzBuzz' 
+
+class Get_FizzFizz(FizzBuzz):
+    def __init__(self):
+        super().__init__(9)
+
+    def get_fizzbuzz(self):
+        return 'FizzFizz'
+
+class Get_BuzzBuzz(FizzBuzz):
+    def __init__(self):
+        super().__init__(25)
+
+    def get_fizzbuzz(self):
+        return 'BuzzBuzz'      
+
+class Get_FizzFizzBuzzBuzz(FizzBuzz):
+    def __init__(self):
+        super().__init__(9,25)
+        
+    def get_fizzbuzz(self):
+        return 'FizzFizzBuzzBuzz'      
 
 
-class Get_allFizzbuzz(FizzBuzz):
-    def get_allfizzbuzz(self):
-        if self.num % 3 == 0 and self.num % 5 == 0:
-            fizzbuzz = Get_FizzBuzz()
-            return fizzbuzz.get_fizzBuzz()
-        elif self.num % 3 == 0:
-            fizz = Get_fizz()
-            return fizz.get_fizz()
-        elif self.num % 5 == 0:
-            buzz = Get_Buzz()
-            return buzz.get_Buzz()
-        else:
-            return 'nofizzbuzz'
-# class Get_fizzbuzzsuper(FizzBuzz):
-#     def get_fizzBuzzsuper(self):
-#         if self.num % 9 == 0 and self.num % 25 == 0:
-#             return 'FizzFizzBuzzBuzz'
-#         elif self.num % 9 == 0:
-#             return 'FizzFizz'
-#         elif self.num % 25 == 0:
-#             return 'BuzzBuzz'
-#         else:
-#             return 'NoFizzBuzz'
-
-def show_fizzbuzz(n: int):
-    show = Get_allFizzbuzz(n)
-    # show.add_number(n)
-    return show.get_allfizzbuzz()
-
-
-print("show_fizzBuzz {0}".format(show_fizzbuzz(15)))
-# def prints():
-#     for i in range(1,100):
-#         if show_fizzbuzz(i) == 'FizzBuzz':
-#             print("{0}: {1}".format(i,show_fizzbuzz(i)))
-
-# prints()
-
-
-# def superFizzBuzz(n):
-#     if n % 3 == 0:
-#         return 'Fizz'
-#     elif n % 5 == 0:
-#         return 'Fizz'
-#     elif n % 3 == 0 and n % 5 == 0:
-#         return 'FizzBuzz'
-#     if n % 9 == 0:
-#         return 'FizzFizz'
-#     elif n % 25 == 0:
-#         return 'BuzzBuzz'
-#     elif n % 9 == 0 and n % 25 == 0:
-#         return 'FizzFizzBuzzBuzz'
-#     elif n % 3 and n % 5 and n % 9 and n % 25:
-#         return 'NoFizzBuzz'
+def super_fizzbuzz(n: int) -> str:
+    shows : list[FizzBuzz] = [
+        Get_FizzFizzBuzzBuzz(),
+        Get_FizzFizz(),
+        Get_BuzzBuzz(),
+        Get_FizzBuzz(),
+        Get_fizz(), 
+        Get_Buzz(),
+        FizzBuzz(1)
+    ] 
+    for show in shows:
+        fizzbuzz = show.call_divition(n)
+        if not fizzbuzz is None:
+            return fizzbuzz
--- superFizzbuzz/test_Superfizzbuzz.py
+++ superFizzbuzz/test_Superfizzbuzz.py
@@ -1,42 +1,81 @@
-from Sufizzbuzz import show_fizzbuzz
+from Sufizzbuzz import super_fizzbuzz
 
 import unittest
 
 class testsuperfizzbuzz(unittest.TestCase):
     def test_fizz_to_number_of_3(self):
         number = 3
-        result = show_fizzbuzz(number)
-        self.assertEquals(result,'Fizz')
+        expeact = 'Fizz'
+        result = super_fizzbuzz(number)
+        self.assertEquals(result,expeact)
     def test_NO_fizz_to_number_of_2(self):
         number = 2
-        result = show_fizzbuzz(number)
-        self.assertEquals(result,'NoFizzBuzz')
+        expeact = 'NoFizzBuzz'
+        result = super_fizzbuzz(number)
+        self.assertEquals(result,expeact)
     def test_Buzz_to_number_of_25(self):
-        number = 25
-        result = show_fizzbuzz(number)
-        self.assertEquals(result,'Buzz')
+        number = 5
+        expeact = 'Buzz'
+        result = super_fizzbuzz(number)
+        self.assertEquals(result,expeact)
     def test_fizzBuzz_to_number_of_15(self):
         number = 15
-        result = show_fizzbuzz(number)
-        self.assertEquals(result,'FizzBuzz')
-    def test_FizzFizz_to_number_of_18(self):
-        number = 9
-        result = show_fizzbuzz(number)
-        self.assertEquals(result,'FizzFizz')
-    def test_BuzzBuzz_to_number_of_50(self):
-        number = 50
-        result = show_fizzbuzz(number)
-        self.assertEquals(result,'BuzzBuzz')
-    def test_FizzFizzBuzzBuzz_number_4725(self):
-        number = 225
-        result = show_fizzbuzz(number)
-        self.assertEqual(result,'FizzFizzBuzzBuzz')
-    def test_Not_FizzFizzBuzzBuzz_number_4725(self):
-        number = 224
-        result = show_fizzbuzz(number)
-        self.assertNotEqual(result,'FizzFizzBuzzBuzz')
+        expeact = 'FizzBuzz'
+        result = super_fizzbuzz(number)
+        self.assertEquals(result,expeact)
+    def test_BuzzBuzz_to_number_of_25(self):
+        number = 25
+        expeact = 'BuzzBuzz'
+        result = super_fizzbuzz(number)
+        self.assertEquals(result,expeact)
     def test_NofizzBuzz_number_1(self):
         number = 1
-        result = show_fizzbuzz(number)
-        self.assertEqual(result,'NoFizzBuzz')
+        expeact = 'NoFizzBuzz'
+        result = super_fizzbuzz(number)
+        self.assertEqual(result,expeact)
+    def test_NofizzBuzz_number_10001(self):
+        number = 10001
+        expeact = 'NoFizzBuzz'
+        result = super_fizzbuzz(number)
+        self.assertEqual(result,expeact)
+    def test_BuzzBuzz_number_10000(self):
+        number = 10000
+        expeact = 'BuzzBuzz'
+        result = super_fizzbuzz(number)
+        self.assertEqual(result,expeact)
+    def test_BuzzBuzz_number_5000(self):
+        number = 5000
+        expeact = 'BuzzBuzz'
+        result = super_fizzbuzz(number)
+        self.assertEqual(result,expeact)
+    def test_Buzz_number_9356(self):
+        number = 9355
+        expeact = 'Buzz'
+        result = super_fizzbuzz(number)
+        self.assertEqual(result,expeact)
+    def test_Fizz_number_9356(self):
+        number = 2499
+        expeact = 'Fizz'
+        result = super_fizzbuzz(number)
+        self.assertEqual(result,expeact)
+    def test_fizz_number_9356(self):
+        number = 5568
+        expeact = 'Fizz'
+        result = super_fizzbuzz(number)
+        self.assertEqual(result,expeact)
+    def test_BuzzBuzz_number_9356(self):
+        number = 7000
+        expeact = 'BuzzBuzz'
+        result = super_fizzbuzz(number)
+        self.assertEqual(result,expeact)
+    def test_give_135_return_fizzfizz(self):
+        num = 135
+        expect = 'FizzFizz'
+        result = super_fizzbuzz(num)
+        self.assertEqual(result, expect)
+    def test_give_6075_return_fizzfizzbuzzbuzz(self):
+        num = 6075
+        expect = 'FizzFizzBuzzBuzz'
+        result = super_fizzbuzz(num)
+        self.assertEqual(result, expect)
    
