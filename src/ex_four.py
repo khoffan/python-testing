a982dc2086a3d6521096df626de4fcecc92cd663
khoffan <khoffan222@gmail.com>
2022-09-28 23:19:35 +0700

    first

--- src/ex_four.py
+++ src/ex_four.py
@@ -5,6 +5,7 @@ def alternate(s): #two-charector
     maxlen = 0
     if len(s) > 1000:
         return -1
+        
     else:
         for a, b in combinations(set(s), 2):
             alts = ''.join([c for c in s if c in (a,b)])
