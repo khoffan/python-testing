8e4b9b458e31f611fb4cbac3209274c7ee46d909
khoffan <khoffan222@gmail.com>
2022-09-28 22:56:46 +0700

    first-commit-to-new-branch

--- src/catandmouses.py
+++ src/catandmouses.py
@@ -1,13 +1,18 @@
-def cat_and_mouse(x: int ,y:int,z:int):
-    distanceA = abs(x - z)
-    distanceB = abs(y - z)
-    if distanceA > distanceB:
-        return 'Cat B'
-    elif distanceA < distanceB:
-        return 'Cat A'
+def cat_and_mouse(x: int, y: int, z: int):
+    if x > 100 or y > 100 or z > 100:
+        return ''
     else:
-        return 'Mouse C'
- 
+        distanceA = abs(x - z)
+        distanceB = abs(y - z)
+
+        if distanceA > distanceB:
+            return 'Cat B'
+        if distanceA < distanceB:
+            return 'Cat A'
+        elif distanceA == distanceB:
+            return 'Mouse C'
+
+
 # if __name__ == '__main__':
 #     line_st = input("Enter a b c: ")
 #     line = map(int,line_st.split())
--- tests/test_catandmouse.py
+++ tests/test_catandmouse.py
@@ -2,22 +2,39 @@ from src.catandmouses import cat_and_mouse
 
 import unittest
 
+
 class Testcatandmouse(unittest.TestCase):
     def test_give_1_2_3_show_cat_B(self):
-        line_st = [1,2,3]
-        line = map(int,line_st)
+        line_st = [1, 2, 3]
+        line = map(int, line_st)
 
         result = cat_and_mouse(*line)
-        self.assertEqual(result,'Cat B')
+        self.assertEqual(result, 'Cat B')
+
     def test_give_1_3_2_show_mouse_c(self):
-        line_st = [1,3,2]
-        line = map(int,line_st)
+        line_st = [1, 3, 2]
+        line = map(int, line_st)
 
         result = cat_and_mouse(*line)
-        self.assertEqual(result,'Mouse C')
+        self.assertEqual(result, 'Mouse C')
+
     def test_give_3_5_2_show_cat_a(self):
-        line_st = [3,5,2]
-        line = map(int,line_st)
+        line_st = [3, 5, 2]
+        line = map(int, line_st)
 
         result = cat_and_mouse(*line)
-        self.assertEqual(result,'Cat A')
+        self.assertEqual(result, 'Cat A')
+
+    def test_give_80_50_200_show_cat_a(self):
+        line_st = [80, 50, 200]
+        line = map(int, line_st)
+
+        result = cat_and_mouse(*line)
+        self.assertEqual(result, '')
+
+    def test_give_60_40_99_show_cat_a(self):
+        line_st = [60, 40, 99]
+        line = map(int, line_st)
+
+        result = cat_and_mouse(*line)
+        self.assertEqual(result, 'Cat A')
